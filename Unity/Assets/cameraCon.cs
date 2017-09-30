d4c021eaf9c9f1cf1690f49db7103a23053f4e6f
Swelsh13 <scott.welsh@hotmail.com>
2017-09-29 23:53:43 -0400

    lab3finish

---
+++ Unity/Assets/cameraCon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraCon : MonoBehaviour {
+	public Transform Player; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player.position.x < -3.04f && Player.position.x > -9.69f) {
+		//green
+			transform.position = new Vector3(Player.position.x, transform.position.y,transform.position.z);
+			Camera.main.orthographicSize = 3;
+		}else if(Player.position.x < 3.71f && Player.position.x > -2.93f){
+			//black
+			transform.position = new Vector3(0,0,transform.position.z);
+			Camera.main.orthographicSize = 5;
+		}else if(Player.position.x < 10.11f && Player.position.x > 3.71f){
+			//red
+			transform.position = new Vector3(Player.position.x, transform.position.y,transform.position.z);
+
+			float distance = Player.position.x - 3.73f;
+			float amount = distance / 6;
+			print (amount);
+			Camera.main.orthographicSize = (amount * 4) + 1 ;
+		}
+	}
+}
---
+++ Unity/Assets/movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public float speed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xInput = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		//print (xInput);
+		transform.position = new Vector3 (transform.position.x + xInput,0,0);
+
+	}
+}
